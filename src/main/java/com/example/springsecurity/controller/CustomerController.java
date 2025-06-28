6063042510d57c5b8ab215a63807cfe3475bd094
hassan <hosseinikhah.hassan@gmail.com>
2025-06-28 14:02:59 +0330

    add db h2

--- src/main/java/com/example/springsecurity/controller/CustomerController.java
+++ src/main/java/com/example/springsecurity/controller/CustomerController.java
@@ -5,9 +5,11 @@ import com.example.springsecurity.service.CustomerService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/customer")
 public class CustomerController {
     private final CustomerService customerService;
     private final PasswordEncoder passwordEncoder;
